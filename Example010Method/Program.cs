ab02ef8b5119b8887324df4d7fcbc020daab9a92
Alex <gmail@alex.com>
2022-11-18 22:19:29 +0300

    ex 10

---
+++ Example010Method/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {132, 21, 453, 24, 15, 26, 78, 128};
+
+int n = array.Length;
+int find = 128;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine (index);
+    }
+    index++;
+}
