fdb9e01c6e68fd48c5b0c09d89c150c64292870d
Alex <gmail@alex.com>
2022-11-21 13:22:18 +0300

    lection 3

---
+++ Methods/Program.cs
@@ -0,0 +1,168 @@
+﻿// Type 1
+
+/*void Method1()
+{
+    Console.WriteLine ("Author ...");
+}
+Method1();
+*/
+
+
+
+//Type2
+/*
+void Method2 (string msg)
+{
+    Console.WriteLine (msg);
+}
+Method2 ("Message ...");
+
+void Method21 (string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine (msg);
+       i++;
+    }
+}
+Method21 (count: 4, msg:"Text ..." );
+*/
+
+
+
+// Type3
+/*
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine (year);
+*/
+
+
+//Type4
+/* 
+string Method4 (int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4 (10, "asdf");
+Console.WriteLine (res);
+*/
+
+/*
+string Method4 (int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4 (10, "asdf");
+Console.WriteLine (res);
+*/
+
+/*
+for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j < 10; j++)
+    {
+        Console.WriteLine ($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine ();
+}
+*/
+
+
+//============Работа с текстом
+// Дан текст. В тексте нужно: все пробелы заменить черточками,
+// Маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+/*
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             0123..
+// s[3] // r
+
+string Replace (string text, char oldValue, char newValue)//value - символ
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace (text,  ' ', '-');
+
+Console.WriteLine (newText);
+Console.WriteLine ();
+
+newText = Replace (newText,  'к', 'К');
+Console.WriteLine (newText);
+Console.WriteLine ();
+
+newText = Replace (newText,  'с', 'С');
+Console.WriteLine (newText);
+*/
+
+
+//найти позицию минимального элемента в неотсортированной части массива
+//произвести обмен этого значения со значением первой неотсортированной позиции
+//повторять пока есть неотсортированные элементы
+
+int[] arr = {1,5,4,2,3,2,6,7 , 1, 1};
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write ($"{array[i]} ");
+    }
+    Console.WriteLine ();
+}
+
+void SelectionSort (int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array [i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray (arr);
+SelectionSort (arr);
+PrintArray (arr);
